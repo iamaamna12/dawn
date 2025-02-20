48c45fcfa20b7ed5545ae2c47d441fd1bd670955
Aamna <aash00001@stud.uni-saarland.de>
2025-02-20 12:05:10 +0100

    update to header

--- assets/hamburger.js
+++ assets/hamburger.js
@@ -1,8 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburgerIcon = document.querySelector('.navbar__hamburger');
-  const hamburgerMenu = document.querySelector('.hamburger-menu');
+// Toggle the menu when hamburger icon is clicked
+const hamburger = document.querySelector('.navbar__hamburger');
+const menu = document.querySelector('.navbar__menu');
 
-  hamburgerIcon.addEventListener('click', function () {
-    hamburgerMenu.classList.toggle('show');
-  });
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('active');
 });
